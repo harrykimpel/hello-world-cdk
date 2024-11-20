d156875a3992ef11d890452b2b2ea6197528812f
Harry Kimpel <harrykimpel@hotmail.com>
2024-11-20 15:22:55 +0100

    Initial commit
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,6 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Import the Lambda module
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {
+  AdotLambdaExecWrapper,
+  AdotLayerVersion,
+  AdotLambdaLayerJavaScriptSdkVersion,
+} from 'aws-cdk-lib/aws-lambda';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +19,42 @@ export class CdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // Define the Lambda function resource
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World!'),
+          };
+        };
+      `),
+      adotInstrumentation: {
+        layerVersion: AdotLayerVersion.fromJavaScriptSdkLayerVersion(AdotLambdaLayerJavaScriptSdkVersion.LATEST),
+        execWrapper: AdotLambdaExecWrapper.REGULAR_HANDLER,
+      },
+      environment: {
+        ADOT_SERVICE_NAME: "hello-world-service",
+        AWS_LAMBDA_EXEC_WRAPPER: "/opt/otel-handler",
+        OTEL_EXPORTER_OTLP_ENDPOINT: "https://otlp.nr-data.net:4317",
+        OTEL_EXPORTER_OTLP_HEADERS: "api-key=NEW_RELIC_LICENSE_KEY",
+        OTEL_SERVICE_NAME: "hello-world-service",
+        OTEL_TRACES_SAMPLER: "always_on",
+        OTEL_LOG_LEVEL: "debug",
+      },
+    });
+
+    // Define the Lambda function URL resource
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    // Define a CloudFormation output for your URL
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+      value: myFunctionUrl.url,
+    })
   }
 }
